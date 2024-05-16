86db82c164dd6c60c856e451bfbee2a74bbb0260
Opilane TTHK <opilane@tthk.ee>
2024-05-16 14:07:16 +0300

    hreht
    eheheeh

--- forloop/Program.cs
+++ forloop/Program.cs
@@ -26,6 +26,39 @@
                 }
                 Console.WriteLine(" ");
             }
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            diamond();
+        }
+        static void diamond()
+        {
+            int i, j, count = 1, number;
+            Console.Write("Enter number of rows:");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine( );
+            count = number - 1;
+            for (j = 1; j <= number; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            count = 1;
+            for (j = 1; j <= number - 1; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - j) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
